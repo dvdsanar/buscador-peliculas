9221774440fc5dfb6b2aabf86582c47f37df7113
David S <dvdsanar@gmail.com>
2022-03-09 18:01:32 +0100

    arreglo .gitignore

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ const app = express();
 app.use(express.json());
 app.listen(3000, () => console.log("Servidor levantado en 3000"));
 app.get("/", (req, res) => res.send("Hello World!"));
+
+console.log(app);
