bc8fa5d011a932f3101f77792b4580465458cd43
DESKTOP-JR2D966\Padres <dvdsanar@gmail.com>
2022-03-16 13:44:28 +0100

    pasar parametros al middleware

--- middleware.js
+++ middleware.js
@@ -1,17 +1,19 @@
 const Usuario = require("./usuarios/usModelo.js");
 const jwt = require("jsonwebtoken");
 
-const verificacion = (req, res, next) => {
-  try {
-    const token = jwt.verify(req.headers.token, "geekshubs");
-    console.log(token);
-    if (token.rol == "admin") {
-      next();
-    } else {
-      res.status(403).send("Tus credenciales no te permiten acceder aqui");
+const verificacion = (comprobacionRol) => {
+  return (req, res, next) => {
+    try {
+      const token = jwt.verify(req.headers.token, "geekshubs");
+      console.log(token);
+      if (token.rol == comprobacionRol) {
+        next();
+      } else {
+        res.status(403).send("Tus credenciales no te permiten acceder aqui");
+      }
+    } catch (error) {
+      res.status(401).send(error);
     }
-  } catch (error) {
-    res.status(401).send(error);
-  }
+  };
 };
 module.exports = verificacion;
--- usuarios/usRutas.js
+++ usuarios/usRutas.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("./usControlador.js");
-const autenticar = require("../middleware.js");
+const verificacion = require("../middleware.js");
 
 //Rutas de todos los verbos de nuestras funciones para los usuarios
-router.get("/", autenticar, controller.traerUsuarios);
+router.get("/", verificacion("cliente"), controller.traerUsuarios);
 router.get("/:id", controller.traerUsuariosID);
 router.post("/", controller.nuevoUsuario);
 router.patch("/:id", controller.modificarParteUsuario);
