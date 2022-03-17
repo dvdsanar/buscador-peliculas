cd13ae18badf1b415f3c64ce4bb6a359aef38b60
DESKTOP-JR2D966\Padres <dvdsanar@gmail.com>
2022-03-17 12:45:57 +0100

    Try / catch añadido al controlador de usuarios

--- usuarios/usControlador.js
+++ usuarios/usControlador.js
@@ -4,13 +4,11 @@ const jwt = require("jsonwebtoken");
 //Función para mostrar los usuarios por su nombre, email o todos los usuarios
 module.exports.traerUsuarios = async (req, res) => {
   try {
-    if (req.query.nombre) {
-      console.log("Entra por nombre");
-      const listaFiltrada = await Usuarios.find({ nombre: req.query.nombre });
-      res.json(listaFiltrada);
-    } else if (req.query.email) {
-      console.log("Entra por email");
-      const listaFiltrada = await Usuarios.find({ email: req.query.email });
+    if (req.query.nombre || req.query.email) {
+      console.log("Entra por nombre o email");
+      const listaFiltrada = await Usuarios.find({
+        $or: [{ nombre: req.query.nombre }, { email: req.query.email }],
+      });
       res.json(listaFiltrada);
     } else {
       console.log("Entra por NADA");
@@ -24,45 +22,66 @@ module.exports.traerUsuarios = async (req, res) => {
 
 //Función para mostrar los usuarios por su id único
 module.exports.traerUsuariosID = async (req, res) => {
-  console.log("Usuarios por id");
-  res.json(await Usuarios.find({ _id: req.params.id }));
+  try {
+    console.log("Usuarios por id");
+    res.json(await Usuarios.find({ _id: req.params.id }));
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 //Función para crear un nuevo usuario
 module.exports.nuevoUsuario = async (req, res) => {
-  const usuarioCreado = {
-    nombre: req.body.nombre,
-    email: req.body.email,
-    password: req.body.password,
-    rol: "cliente",
-  };
-  const usuario = new Usuarios(usuarioCreado);
-  await usuario.save();
-  res.json(usuario);
+  try {
+    const usuarioCreado = {
+      nombre: req.body.nombre,
+      email: req.body.email,
+      password: req.body.password,
+      rol: "cliente",
+    };
+    const usuario = new Usuarios(usuarioCreado);
+    await usuario.save();
+    res.json(usuario);
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 //Función para modificar alguna de las claves del usuario
 module.exports.modificarParteUsuario = async (req, res) => {
-  await Usuarios.updateOne({ _id: req.params.id }, req.body);
-  res.status(200).send("Usuario modificado");
+  try {
+    await Usuarios.updateOne({ _id: req.params.id }, req.body);
+    res.status(200).send("Usuario modificado");
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 //Función para borrar un usuario concreto a traves de su id
 module.exports.borrarUsuario = async (req, res) => {
-  console.log("Borrando un usuario");
-  res.json(await Usuarios.deleteOne({ _id: req.params.id }));
+  try {
+    console.log("Borrando un usuario");
+    await Usuarios.deleteOne({ _id: req.params.id });
+    res.json("Usuario eliminado");
+  } catch (error) {
+    res.json(error);
+  }
 };
 
 //Función para generar un token
 module.exports.generarFicha = async (req, res) => {
-  const buscarUsuario = await Usuarios.findOne({
-    email: req.headers.email,
-    password: req.headers.password,
-  });
-  if (buscarUsuario) {
-    const ficha = jwt.sign({ rol: buscarUsuario.rol }, "geekshubs");
-    res.json(ficha);
-  } else {
-    res.status(401).send("No tienes un usuario para poder pasar");
+  try {
+    const buscarUsuario = await Usuarios.findOne({
+      email: req.headers.email,
+      password: req.headers.password,
+    });
+    if (buscarUsuario) {
+      const ficha = jwt.sign({ rol: buscarUsuario.rol }, "geekshubs");
+      res.json(ficha);
+    } else {
+      res.status(401).send("No tienes un usuario para poder pasar");
+    }
+  } catch (error) {
+    res.json(error);
   }
 };
